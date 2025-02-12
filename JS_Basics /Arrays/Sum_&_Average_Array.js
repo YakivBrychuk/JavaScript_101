c424454f94404a5d736c64a5a83792c0a771c460
Yakiv Brychuk <147402437+YakivBrychuk@users.noreply.github.com>
2025-02-12 10:10:53 +0000

    Update Arrays notes

--- JS_Basics /Arrays/Sum_&_Average_Array.js
+++ JS_Basics /Arrays/Sum_&_Average_Array.js
@@ -1,6 +1,16 @@
 var arr = [2,2,4,5,6,7,8,9,10];
 
-// Sum of the elements of the array
+// sum of the elements of the array
+function simpleArraySum(arr) {
+    // Write your code here
+    let sum = 0;
+    let summ = (item) => sum+=item;
+    ar.forEach(summ)
+    
+    return sum;
+}
+
+// Sum of the elements of the array with anonymous function and forEach method
 let sum = 0;
 arr.forEach(element => {sum += element});
 
