c47758286b541bcaec897abef594c22104cc35f9
Yakiv Brychuk <147402437+YakivBrychuk@users.noreply.github.com>
2025-02-14 16:38:19 +0000

    Practice js + html

---
+++ Functions/User_Input/User_Input.js
@@ -0,0 +1,10 @@
+
+// let username;
+
+// username = window.prompt("What is your username: ");
+
+// console.log(username);
+
+
+document.getElementById("myH1").textContent = `Hello`;
+document.getElementById("myP").textContent = `I love Tofu`;
